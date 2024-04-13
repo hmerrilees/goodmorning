e55f48654d88db24380350d74a5f612c5eff2124
Henry Merrilees <henry@merrile.es>
2024-04-13 00:23:14 -0700

    typo

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 //! A morning greeting cli that uses the Wibold of Cambrai's Ludus Claricalis to determine the virtue of the day.
 //! Enumeration of virtues and dice combinations is taken from Donald Knuth's "The Art of Computer Programming, Volume 4A" on page 493.
 
-use crate::ludis_claricalis::{LudusClaricalis, Output as LCOutput};
+use crate::ludus_claricalis::{LudusClaricalis, Output as LCOutput};
 use clap::Parser;
 
 pub mod ludis_claricalis;
